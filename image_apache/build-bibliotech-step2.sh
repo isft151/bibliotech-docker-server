5d4c27c27acd864923351173250d43c1f6a18db8
gabrielinuz <gabrielinuz@gmail.com>
2022-07-13 00:12:47 -0300

    organize

--- image_apache/build-bibliotech-step2.sh
+++ image_apache/build-bibliotech-step2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod -R 777 ./htdocs/bibliotech &&
-chmod +x ./htdocs/bibliotech/version3.0/build.sh &&
-cd ./htdocs/bibliotech/version3.0/ && 
+chmod +x ./htdocs/bibliotech/build.sh &&
+cd ./htdocs/bibliotech/ && 
 ./build.sh
